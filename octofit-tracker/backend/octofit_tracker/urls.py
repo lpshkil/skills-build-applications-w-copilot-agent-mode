46fcc6a5d88628ab65575f2aecc5cea64ad26b19
Liubomyr Shkil <48146752+lpshkil@users.noreply.github.com>
2025-11-24 09:32:31 +0000

    fix(octofit): codespace/localhost API URL and allowed hosts for Django backend

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from .views import TeamViewSet, UserViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet, api_root
+import os
+
 
 router = routers.DefaultRouter()
 router.register(r'teams', TeamViewSet)
@@ -25,6 +27,18 @@ router.register(r'users', UserViewSet)
 router.register(r'activities', ActivityViewSet)
 router.register(r'workouts', WorkoutViewSet)
 router.register(r'leaderboard', LeaderboardViewSet)
+
+# Patch DRF reverse to use codespace host if present
+from rest_framework.reverse import reverse as drf_reverse
+def codespace_reverse(viewname, args=None, kwargs=None, request=None, format=None, **extra):
+    url = drf_reverse(viewname, args=args, kwargs=kwargs, request=request, format=format, **extra)
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name and request is not None:
+        url = url.replace('http://localhost:8000', f'https://{codespace_name}-8000.app.github.dev')
+        url = url.replace('http://127.0.0.1:8000', f'https://{codespace_name}-8000.app.github.dev')
+    return url
+import sys
+sys.modules['octofit_tracker.urls_reverse'] = type('urls_reverse', (), {'reverse': codespace_reverse})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
